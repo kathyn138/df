88a2ff11130e5331e8e8d53355220e026f028aad
Kathy Nguyen <kathyn262@gmail.com>
2024-06-26 11:30:33 -0700

    adjusted navbar animation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   setTimeout(function () {
     $('.brand-rest, .brand-rest').animate(
       { opacity: 0 },
-      500,
+      3000,
       function () {
         $(this).hide();
         $('.navbar-brand').html('DF');
