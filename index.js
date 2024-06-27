4cc34d94580fca1629b35ebbcad20e2696a0c3e6
Kathy Nguyen <kathyn262@gmail.com>
2024-06-27 16:58:54 -0700

    fin navbar brand animation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-  // NAVBAR ANIMATION
+  // NAVBAR ANIMATIONS
+  
+  // to remove rest of branding
   setTimeout(function () {
-    $('.brand-rest, .brand-rest').animate({ opacity: 0 }, 3000, function () {
-      $(this).hide();
-      $('.navbar-brand').html('DF');
-    });
+    $('.brand-rest, .brand-rest').animate({ opacity: 0 }, 3000);
   }, 500);
+  
+  // to shift last initial closer to first initial on left
+  setTimeout(function() {
+    $('.brand-initial-last').animate({marginLeft: '-40px'}, 1000)
+  }, 3500)
 });
