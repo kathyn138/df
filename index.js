63ac246816f36643ab049ca461cd17898812d683
Kathy Nguyen <kathyn262@gmail.com>
2024-06-28 11:18:47 -0700

    set initial unmute always

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ $(document).ready(function () {
   setTimeout(function() {
     $('.brand-initial-last').animate({marginLeft: '-40px'}, 1000)
   }, 3500)
+
+  const player = new playerjs.Player(document.getElementById('promo'));
+
+  // always keep video on mute on landing page initially to keep autoplay
+  // default is that player will remember unmute state ie blocks autoplay if left unmuted
+  player.on('ready', () => {
+      player.mute();
+  });
 });
