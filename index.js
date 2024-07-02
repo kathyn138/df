0bed29943a41702cff7275969dbc937731a24ef9
Kathy Nguyen <kathyn262@gmail.com>
2024-07-01 18:00:49 -0700

    subbed diff video on viewports

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ $(document).ready(function () {
       $('.navbar-nav').unwrap();
     }
 
+    // if not mobile view, use desktop version of video
+    if ($('#promo').attr('src') !== 'https://iframe.mediadelivery.net/embed/261546/dc276456-7fe6-403a-b903-d9044f5355e3?autoplay=true&loop=false&muted=true&preload=true&responsive=true') {
+      $('#promo').attr(
+        'src',
+        'https://iframe.mediadelivery.net/embed/261546/dc276456-7fe6-403a-b903-d9044f5355e3?autoplay=true&loop=false&muted=true&preload=true&responsive=true'
+      );
+    }
+
     // DF on top left for desktops
     if (window.matchMedia('(min-width: 992px)').matches) {
       console.log('here');
@@ -54,6 +62,8 @@ $(document).ready(function () {
         'src',
         'https://iframe.mediadelivery.net/embed/263606/5e2e3857-2aff-4f4e-8689-8ad384b028de?autoplay=true&loop=false&muted=true&preload=true&responsive=true'
       );
+
+      $('.social-icons').removeClass('justify-content-end').addClass('justify-content-center');
     }
   });
 
@@ -69,6 +79,8 @@ $(document).ready(function () {
       'src',
       'https://iframe.mediadelivery.net/embed/263606/5e2e3857-2aff-4f4e-8689-8ad384b028de?autoplay=true&loop=false&muted=true&preload=true&responsive=true'
     );
+
+    $('.social-icons').removeClass('justify-content-end mt-3').addClass('justify-content-center');
   }
 
   const player = new playerjs.Player(document.getElementById('promo'));
