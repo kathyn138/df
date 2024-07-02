25819ca3e5902bba14eb1ee94a06534116af3f55
Kathy Nguyen <kathyn262@gmail.com>
2024-07-01 17:36:01 -0700

    fin mobile video player

--- index.js
+++ index.js
@@ -9,47 +9,66 @@ $(document).ready(function () {
       visibility: 'visible',
     });
     $('.brand').css({
-      animation: 'none'
+      animation: 'none',
     });
     $('.brand-rest').css({
       animation: 'none',
-      display: 'none'
+      display: 'none',
     });
     $('.brand-initial-last').css({
       animation: 'none',
-      marginLeft: '-15px'
+      marginLeft: '-15px',
     });
     $('.brand-initial-first').css({
-      animation: 'none'
+      animation: 'none',
     });
+
+    // if uncollapsed parent navbar is there then remove
+    // otherwise will be present after going from
+    // collapsed viewport to non collapsed viewport
+    if ($('#navbarSupportedContent').length) {
+      $('.navbar-nav').unwrap();
+    }
 
     // DF on top left for desktops
     if (window.matchMedia('(min-width: 992px)').matches) {
       console.log('here');
-      $('.brand').css({left: '1%'});
+      $('.brand').css({ left: '1%' });
     }
 
     // center DF for medium devices
-    if (window.matchMedia('(max-width: 992px) and (min-width: 768px)').matches) {
-      console.log('between')
-      $('.brand').css({left: '0%'});
+    if (
+      window.matchMedia('(max-width: 992px) and (min-width: 768px)').matches
+    ) {
+      $('.brand').css({ left: '0%' });
+    }
+
+    if (window.matchMedia('(max-width: 576px)').matches) {
+      $('.navbar-nav').wrap(
+        '<div class="collapse navbar-collapse" id="navbarSupportedContent"></div>'
+      );
+
+      // switch video player for mobile device
+      // allows volume button to appear
+      $('#promo').attr(
+        'src',
+        'https://iframe.mediadelivery.net/embed/263606/5e2e3857-2aff-4f4e-8689-8ad384b028de?autoplay=true&loop=false&muted=true&preload=true&responsive=true'
+      );
     }
   });
 
-  
+  // mobile devices
+  if (window.matchMedia('(max-width: 576px)').matches) {
+    $('.navbar-nav').wrap(
+      '<div class="collapse navbar-collapse" id="navbarSupportedContent"></div>'
+    );
 
-  if ($(window).width < 992 && $(window).width() > 768) {
-    // $('.brand').css({left: '0%'});
-    // $('#navbar').addClass('flex-column');
-    // $('.brand').css({position: 'relative', left: 0});
-    // setTimeout(function() {
-    //   $('.brand-initial-last').animate({marginLeft: '-10px'}, 1000)
-    // }, 3500)
-    // setTimeout(function () {
-    //   $('.brand-rest').hide();
-    //   $('.brand-initial-first').css({ marginRight: '-7px' });
-    //   $('.brand-initial-last').css({ marginLeft: '-7px' });
-    // }, 3500);
+    // switch video player for mobile device
+    // allows volume button to appear
+    $('#promo').attr(
+      'src',
+      'https://iframe.mediadelivery.net/embed/263606/5e2e3857-2aff-4f4e-8689-8ad384b028de?autoplay=true&loop=false&muted=true&preload=true&responsive=true'
+    );
   }
 
   const player = new playerjs.Player(document.getElementById('promo'));
