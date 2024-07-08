7a68ad60a3cef877f308a49e5e1fbfd773bf0ccb
Kathy Nguyen <kathyn262@gmail.com>
2024-07-08 12:41:16 -0700

    regrouped navbar ani

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
   // NAVBAR ANIMATIONS
 
+  function navBarCollapse() {
+    $('.navbar-default').wrap('<div class="collapse navbar-collapse"></div>');
+    $('.navbar-mobile').wrap(
+      '<div class="collapse navbar-collapse" id="navbarSupportedContent"></div>'
+    );
+
+    $('#navbar-open').on('click touch', function () {
+      $('.navbar-mobile').css({ display: 'block' });
+
+      $('#navbar-open').css({ display: 'none' });
+      $('#navbar-close').css({ display: 'inline' });
+    });
+
+    $('#navbar-close').on('click touch', function () {
+      $('#navbar-close').css({ display: 'none' });
+      $('#navbar-open').css({ display: 'inline' });
+    });
+  }
+
   $(window).resize(function () {
     // mobile devices
     if (window.matchMedia('(max-width: 576px)').matches) {
@@ -8,10 +27,7 @@ $(document).ready(function () {
         .removeClass('justify-content-end')
         .addClass('justify-content-center');
 
-      $('.navbar-default').wrap('<div class="collapse navbar-collapse"></div>');
-      $('.navbar-mobile').wrap(
-        '<div class="collapse navbar-collapse" id="navbarSupportedContent"></div>'
-      );
+      navBarCollapse();
     }
   });
 
@@ -21,23 +37,6 @@ $(document).ready(function () {
       .removeClass('justify-content-end mt-3')
       .addClass('justify-content-center');
 
-    $('.navbar-default').wrap('<div class="collapse navbar-collapse"></div>');
-    $('.navbar-mobile').wrap(
-      '<div class="collapse navbar-collapse" id="navbarSupportedContent"></div>'
-    );
-
-    $('#navbar-open').on('click touch', function() {
-      $('.navbar-mobile').css({display: 'block'})
-
-      $('#navbar-open').css({display: 'none'});
-      $('#navbar-close').css({display: 'inline'});
-
-    });
-
-    $('#navbar-close').on('click touch', function() {
-      // $('.navbar-mobile').css({display: 'none'})
-      $('#navbar-close').css({display: 'none'});
-      $('#navbar-open').css({display: 'inline'});
-    });
+    navBarCollapse();
   }
 });
