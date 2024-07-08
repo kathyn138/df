8ed69c853fbdf8a4207e2e076e810a4713017963
Kathy Nguyen <kathyn262@gmail.com>
2024-07-08 12:30:37 -0700

    updated navbar for main styling

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
       $('.social-icons')
         .removeClass('justify-content-end')
         .addClass('justify-content-center');
+
+      $('.navbar-default').wrap('<div class="collapse navbar-collapse"></div>');
+      $('.navbar-mobile').wrap(
+        '<div class="collapse navbar-collapse" id="navbarSupportedContent"></div>'
+      );
     }
   });
 
@@ -15,5 +20,24 @@ $(document).ready(function () {
     $('.social-icons')
       .removeClass('justify-content-end mt-3')
       .addClass('justify-content-center');
+
+    $('.navbar-default').wrap('<div class="collapse navbar-collapse"></div>');
+    $('.navbar-mobile').wrap(
+      '<div class="collapse navbar-collapse" id="navbarSupportedContent"></div>'
+    );
+
+    $('#navbar-open').on('click touch', function() {
+      $('.navbar-mobile').css({display: 'block'})
+
+      $('#navbar-open').css({display: 'none'});
+      $('#navbar-close').css({display: 'inline'});
+
+    });
+
+    $('#navbar-close').on('click touch', function() {
+      // $('.navbar-mobile').css({display: 'none'})
+      $('#navbar-close').css({display: 'none'});
+      $('#navbar-open').css({display: 'inline'});
+    });
   }
 });
