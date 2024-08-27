17c771bec1c5e752842cb69d2f6d8108d70148c1
Kathy Nguyen <kathyn262@gmail.com>
2024-08-27 16:29:12 -0700

    added create form submission

--- main.js
+++ main.js
@@ -86,4 +86,39 @@ $(document).ready(function () {
       'email=' + document.getElementById('input-newsletter-email').value;
     http.send(params);
   });
+
+  $('#create-form').submit(function (e) {
+    e.preventDefault();
+    let form = e.target;
+    let http = new XMLHttpRequest();
+    http.open('POST', form.action);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.onreadystatechange = function () {
+      if (http.readyState === 4 && http.status === 200) {
+        form.reset();
+        let nameInput = form.querySelector('#create-name-input');
+        let contactInput = form.querySelector('#create-contact-input');
+        let msgInput = form.querySelector('#create-msg-input');
+        let submitBtn = form.querySelector('.create-submit-btn');
+
+        nameInput.style.visibility = 'hidden';
+        contactInput.style.visibility = 'hidden';
+        msgInput.style.visibility = 'hidden';
+        submitBtn.style.visibility = 'hidden';
+
+        let thankYouMessage = form.querySelector('.create-submission-msg');
+
+        thankYouMessage.style.visibility = 'visible';
+      }
+    };
+    let params =
+      'name=' +
+      document.getElementById('create-name-input').value +
+      '&contact-info=' +
+      document.getElementById('create-contact-input').value +
+      '&message=' +
+      document.getElementById('create-msg-input').value;
+
+    http.send(params);
+  });
 });
